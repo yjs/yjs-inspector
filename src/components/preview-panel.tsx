4c147fe36e22f7ca947c90a210ef3ed9f6a6824a
lawvs <18554747+lawvs@users.noreply.github.com>
2024-04-06 16:17:37 +0800

    feat: add json viewer

--- src/components/preview-panel.tsx
+++ src/components/preview-panel.tsx
@@ -1,7 +1,61 @@
-import { Button } from "./ui/button";
+import { defineDataType, JsonViewer, objectType } from "@textea/json-viewer";
 import { Bug } from "lucide-react";
+import * as Y from "yjs";
+import { useYDoc } from "../state";
+import { useTheme } from "./theme-provider";
+import { Button } from "./ui/button";
+
+const yMapType = defineDataType<object>({
+  ...objectType,
+  is(value) {
+    return typeof value === "object" && value instanceof Y.Map;
+  },
+  PreComponent: ({ ...props }) => {
+    const PreComponent = objectType.PreComponent!;
+    return (
+      <span>
+        YMap <PreComponent {...props}></PreComponent>
+      </span>
+    );
+  },
+});
+
+const yArrayType = defineDataType<object>({
+  ...objectType,
+  is(value) {
+    return typeof value === "object" && value instanceof Y.Array;
+  },
+  PreComponent: ({ ...props }) => {
+    const PreComponent = objectType.PreComponent!;
+    return (
+      <span>
+        YArray <PreComponent {...props}></PreComponent>
+      </span>
+    );
+  },
+});
+
+const yTextType = defineDataType<object>({
+  ...objectType,
+  is(value) {
+    return typeof value === "object" && value instanceof Y.Text;
+  },
+  PreComponent: ({ ...props }) => {
+    const PreComponent = objectType.PreComponent!;
+    return (
+      <span>
+        YText <PreComponent {...props}></PreComponent>
+      </span>
+    );
+  },
+});
+
+const valueTypes = [yMapType, yArrayType, yTextType];
 
 export function PreviewPanel() {
+  const { theme } = useTheme();
+  const [yDoc] = useYDoc();
+  yDoc.get;
   return (
     <div className="flex flex-1 flex-col">
       <div className="flex">
@@ -16,6 +70,17 @@ export function PreviewPanel() {
             Report issue
           </a>
         </Button>
+      </div>
+
+      <div className="flex-1 rounded-md">
+        <JsonViewer
+          className="p-2"
+          value={yDoc}
+          rootName="YDoc"
+          theme={theme === "system" ? "auto" : theme}
+          defaultInspectDepth={1}
+          valueTypes={valueTypes}
+        />
       </div>
     </div>
   );
